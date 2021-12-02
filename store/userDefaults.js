9ae62a9ea0f45dc5b84d72f72fc4f972b0aed827
Wind <Wind983571@gmail.com>
2021-12-02 21:29:20 +0400

    增加新的功能

---
+++ store/userDefaults.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const defaults = new Vuex.Store({
+	state: {//对数据的全局存储
+		isLogin: false,
+	}
+})
+
+export default {
+	defaults
+}
---
+++ store/userStorage.js
@@ -0,0 +1,18 @@
+
+const storageKey = {
+	isLogin: "isLogin", // 是否登录
+}
+
+const storage = {
+	getIsLogin() {
+		return uni.getStorageSync(storageKey.isLogin)
+	}
+	setIsLogin(isLogin) {
+		uni.setStorageSync(storageKey.isLogin, isLogin)
+	}
+}
+
+export default {
+	storageKey,
+	storage
+}
