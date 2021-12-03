f9417d91b8dc693122df1d4847c7d6ec8d8808a0
Wind <Wind983571@gmail.com>
2021-12-03 18:43:12 +0400

    增加部分代码

---
+++ store/userDefines.js
@@ -0,0 +1,19 @@
+
+const notifyKey = {
+
+}
+
+const storageKey = {
+
+}
+
+const defaultsKey = {
+
+}
+
+export default {
+	notifyKey,
+	storageKey,
+	defaultsKey
+}
+
---
+++ utils/netDefines.js
@@ -0,0 +1,9 @@
+
+// api 地址
+const api = {
+	startAdvert: "/api/start/ad", // 启动页广告
+}
+
+export default {
+	api
+};
