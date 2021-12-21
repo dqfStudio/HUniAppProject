c94c9dbc494f80405775e03da23e0aa8a6e3a9f6
Wind <Wind983571@gmail.com>
2021-12-21 10:28:51 +0400

    增加新的功能

---
+++ pages/index/HPager.vue
@@ -0,0 +1,49 @@
+<template>
+	<!-- 导航栏 -->
+	<!-- <view v-if="isNav" :style="{height:navHeight + 'px'}">
+		<slot name="nav" />
+	</view>
+	
+	<image v-if="backgroundImage.lenght" class="HPagerImageClass" mode="widthFix" :style="{'width': width+'px', 'src': backgroundImage}">
+		<slot />
+	</image>
+	<view v-else class="HPagerClass" :style="{'width': width+'px', 'height': height+'px', 'background-color': backgroundColor}">
+		<slot />
+	</view> -->
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				width: 0,
+				height: 0
+			}
+		},
+		props: {
+			backgroundColor: {
+				type: String,
+				default: '#FFFFFF'
+			},
+			backgroundImage: {
+				type: String,
+				default: ''
+			}
+		},
+		created() {
+			this.width  = uni.getSystemInfoSync().windowWidth;
+			this.height = uni.getSystemInfoSync().windowHeight;
+		}
+	}
+</script>
+
+<style>
+	.HPagerClass {
+		display: flex;
+		flex-direction: column;
+	}
+	.HPagerImageClass {
+		position: absolute;
+		z-index: 10;
+	}
+</style>
---
+++ pages/index/HScrollPager.vue
@@ -0,0 +1,44 @@
+<template>
+	<!-- <image v-if="backgroundImage.lenght" class="HPagerImageClass" mode="widthFix" :style="{'width': width+'px', 'src': backgroundImage}">
+		<slot name="pageContent" />
+	</image>
+	<scroll-view v-else class="HPagerClass" :style="{'width': width+'px', 'height': height+'px', 'background-color': backgroundColor}">
+		<slot name="pageContent" />
+	</scroll-view> -->
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				width: 0,
+				height: 0
+			}
+		},
+		props: {
+			backgroundColor: {
+				type: String,
+				default: '#FFFFFF'
+			},
+			backgroundImage: {
+				type: String,
+				default: ''
+			}
+		},
+		created() {
+			this.width  = uni.getSystemInfoSync().windowWidth;
+			this.height = uni.getSystemInfoSync().windowHeight;
+		}
+	}
+</script>
+
+<style>
+	.HPagerClass {
+		display: flex;
+		flex-direction: column;
+	}
+	.HPagerImageClass {
+		position: absolute;
+		z-index: 10;
+	}
+</style>
